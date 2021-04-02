09b8bac5fa1e42d2ed146795605fbd81902ee771
Joseph Heetel <joseph.a.heetel@stud.hs-kempten.de>
2021-04-02 18:02:11 +0200

    Fixed Cmakelists.txt files, added external libraries, added licenses folder for 3rd party library licenses  - realtimertfilters-app.cpp is buildable now

---
+++ licenses/GLM - License Notes.md
@@ -0,0 +1,2 @@
+GLM does not supply a separate license file. GLMs Readme.md however specifies the following:
+> The source code and the documentation are licensed under both the [Happy Bunny License (Modified MIT) or the MIT License](manual.md#section0).
--- source/realtimertfilters/realtimertfilters-app/realtimertfilters-app.cpp
+++ source/realtimertfilters/realtimertfilters-app/realtimertfilters-app.cpp
@@ -2,11 +2,34 @@
 //
 
 #include "realtimertfilters-app.h"
+#include <VulkanRaytracingSample.h>
 
-using namespace std;
-
-int main()
+class Test : VulkanRaytracingSample
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+public:
+
+	// Inherited via VulkanRaytracingSample
+	virtual void render() override
+	{}
+};
+
+#if WIN32
+int WINAPI wWinMain(
+	_In_		HINSTANCE	hInstance,
+	_In_opt_	HINSTANCE	hPrevInstance,
+	_In_		LPWSTR		lpCmdLine,
+	_In_		int			nShowCmd
+)
+{
+	return 420;
 }
+#else
+int main(
+	int		argc, 
+	char*	argv[]
+)
+{
+	return 420;
+}
+#endif
+
