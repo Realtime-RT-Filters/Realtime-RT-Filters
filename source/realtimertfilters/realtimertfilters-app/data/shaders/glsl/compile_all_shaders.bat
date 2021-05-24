2ce5e8084ca7a3658ce70983d1913d6400f88257
Joseph Heetel <joseph.a.heetel@stud.hs-kempten.de>
2021-05-24 19:11:16 +0200

    Added compile_all_shaders.bat to gitignore
    This file is generated by cmake for the machine specifically!

--- source/realtimertfilters/realtimertfilters-app/data/shaders/glsl/compile_all_shaders.bat
+++
@@ -1,3 +0,0 @@
-C:\VulkanSDK\1.2.176.1/Bin/glslc.exe prepass/rasterprepass.frag -o prepass/rasterprepass.frag.spv --target-env=vulkan1.2
-C:\VulkanSDK\1.2.176.1/Bin/glslc.exe prepass/rasterprepass.vert -o prepass/rasterprepass.vert.spv --target-env=vulkan1.2
-
