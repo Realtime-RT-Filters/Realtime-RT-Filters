7691227771ab9776641c170c8dc16045c7e60e21
Joseph Heetel <joseph.a.heetel@stud.hs-kempten.de>
2021-05-24 15:09:15 +0200

    Changed shader batch file process to use local vulkan glsl compiler

--- source/realtimertfilters/realtimertfilters-app/data/shaders/glsl/compile_all_shaders.bat
+++ source/realtimertfilters/realtimertfilters-app/data/shaders/glsl/compile_all_shaders.bat
@@ -1,2 +1,3 @@
-glslangValidator prepass/rasterprepass.vert -o prepass/rasterprepass.vert.spv --target-env vulkan1.2
-glslangValidator prepass/rasterprepass.frag -o prepass/rasterprepass.frag.spv --target-env vulkan1.2
+S:\Projects\Programming\SDKs\Vulkan-1.2.170.0/Bin/glslc.exe prepass/rasterprepass.frag -o prepass/rasterprepass.frag.spv --target-env=vulkan1.2
+S:\Projects\Programming\SDKs\Vulkan-1.2.170.0/Bin/glslc.exe prepass/rasterprepass.vert -o prepass/rasterprepass.vert.spv --target-env=vulkan1.2
+
